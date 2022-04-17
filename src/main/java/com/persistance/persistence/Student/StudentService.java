c64498b27347308a71ba40b75e6d91f73e39f2be
l3miage-zodjihok <hamegan252@gmail.com>
2022-04-17 03:47:32 +0200

    supression et moise a jour des etudiant avec controle sur la redondance des données

--- src/main/java/com/persistance/persistence/Student/StudentService.java
+++ src/main/java/com/persistance/persistence/Student/StudentService.java
@@ -3,9 +3,11 @@ package com.persistance.persistence.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -28,6 +30,28 @@ public class StudentService {
             throw  new IllegalStateException("le mail exist deja");
         }
         studentRepository.save(student);
+
+
+    }
+
+    public void deleteStudentById(Long studentId) {
+        boolean studentExist= studentRepository.existsById(studentId);
+        if(!studentExist){
+            throw new IllegalStateException("the student with ID "+studentId+ " don't exist");
+        }
+        studentRepository.deleteById(studentId);
+
+    }
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(()->new IllegalStateException("student with id "+studentId+" does not exist"));
+
+
+        if(name!=null && name.length()>0 && !Objects.equals(student.getName(),name)){student.setName(name);
+        }
+
+            if(email!=null &&  email.length()>0 && !Objects.equals(student.getEmail(),email)){student.setEmail(email);}
 
 
     }
